401e8f330f9e084dd61584e43730b34b1e10a105
Blazeiscoding <nikhilrathore52297@gmail.com>
2024-03-10 23:55:31 +0530

    authirization added

---
+++ 12Blog/src/appwrite/auth.js
@@ -0,0 +1,66 @@
+import conf from "../conf/conf.js";
+
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectid);
+
+        this.account = new Account(this.client);
+        
+    }
+
+    async createAccount({email, password , name}){
+        try {
+           const userAccount =  await this.account.create(ID.unique(),email, password , name);
+           if (userAccount) {
+                // call another method 
+                return this.login({email, password});
+
+           } else {
+            return userAccount;
+           }
+        } catch (error) {
+            throw error;
+        }
+
+    }
+
+
+    async login({email,password}){
+        try {
+            return  await this.account.createEmailSession(email,password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+
+        return null;
+    }
+
+
+    async logout(){
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            throw error ;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default AuthService;
