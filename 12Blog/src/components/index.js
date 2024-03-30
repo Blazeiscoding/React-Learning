50bee2449232889b948c6ae94475ea747b5d7bd0
Blazeiscoding <nikhilrathore52297@gmail.com>
2024-03-31 00:42:04 +0530

    Added More functionality

--- 12Blog/src/components/index.js
+++ 12Blog/src/components/index.js
@@ -3,9 +3,11 @@ import Footer from "./Footer/Footer";
 import Container from './Container/Container'
 import Logo from "./Logo";
 import Logoutbtn from "./Header/Logoutbtn";
+import RTE from "./RTE";
 export {
     Header,
     Footer,
     Container,
-    Logo
+    Logo,
+    RTE
 }
